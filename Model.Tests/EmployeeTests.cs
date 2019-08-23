c4347d744c386f677b7cbfe36926c7ce2dbd6b46
Schpenn <espenlil@stud.ntnu.no>
2019-08-23 08:46:50 +0200

    Added Test folder and employee test

---
+++ Model.Tests/EmployeeTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tests
+{
+    [TestFixture]
+    public class EmployeeTests
+    {
+        [Test]
+        public void ReturnsFullName()
+        {
+            var sut = new Employee()
+            {
+                First_Name = "Espen",
+                Last_Name = "Bjørnsen"
+            };
+
+            Assert.That(sut.FullName, Is.EqualTo("Espen Bjørnsen"));
+
+
+        }
+    }
+}
