dcf6bd65a29e3c866cfae06e1c361e5d47f86534
peder.birkeland <peder.birkeland@gmail.com>
2019-08-23 13:55:04 +0200

    Added some error handling on employee configurator, added static class to access window and application references globally

---
+++ View/Environment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestApp
+{
+    public static class Environment
+    {
+        public static Application Application { get; set; }
+        public static Window Window { get; set; }
+
+    }
+}
